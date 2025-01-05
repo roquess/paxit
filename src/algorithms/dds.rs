9897723a9ba893610788721be68fb2099c8195bc
Steve Roques <steve.roques@gmail.com>
2025-01-05 19:01:20 +0000

    adding dds, farbfeld, hdr, ico, pnm, tga and webp image format

---
+++ src/algorithms/dds.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+use std::io::{self, BufWriter};
+use std::path::Path;
+use image::{open, ImageFormat};
+use crate::pack::Pack;
+
+pub struct Dds;
+
+impl Pack for Dds {
+    fn pack(&self, files: Vec<&Path>, output: &Path) -> io::Result<()> {
+        if files.len() != 1 {
+            return Err(io::Error::new(io::ErrorKind::InvalidInput, "DDS compression supports only one input file"));
+        }
+
+        let input_file = files[0];
+        let img = open(input_file).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+        let mut output_file = BufWriter::new(File::create(output)?);
+        img.write_to(&mut output_file, ImageFormat::Dds).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+        Ok(())
+    }
+
+    fn unpack(&self, _file: &Path, _output_dir: &Path) -> io::Result<()> {
+        Err(io::Error::new(io::ErrorKind::Unsupported, "Unpacking DDS is not implemented"))
+    }
+
+    fn extensions(&self) -> Vec<&'static str> {
+        vec!["dds"]
+    }
+}
+
--- src/algorithms/mod.rs
+++ src/algorithms/mod.rs
@@ -1,10 +1,17 @@
 pub mod bmp;
+pub mod dds;
+pub mod farbfeld;
 pub mod gif;
+pub mod hdr;
+pub mod ico;
 pub mod jpeg;
 pub mod lzma;
 pub mod png;
+pub mod pnm;
 pub mod tar;
+pub mod tga;
 pub mod tiff;
+pub mod webp;
 pub mod zip;
 pub mod zstd;
 
@@ -20,12 +27,19 @@ pub fn determine_compressor(mode: &str, files: &[&Path]) -> Result<Box<dyn Pack>
 
     let compressors = vec![
         Box::new(bmp::Bmp) as Box<dyn Pack>,
+        Box::new(dds::Dds) as Box<dyn Pack>,
+        Box::new(farbfeld::Farbfeld) as Box<dyn Pack>,
         Box::new(gif::Gif) as Box<dyn Pack>,
+        Box::new(hdr::Hdr) as Box<dyn Pack>,
+        Box::new(ico::Ico) as Box<dyn Pack>,
         Box::new(jpeg::Jpeg) as Box<dyn Pack>,
         Box::new(lzma::Lzma) as Box<dyn Pack>,
         Box::new(png::Png) as Box<dyn Pack>,
+        Box::new(pnm::Pnm) as Box<dyn Pack>,
         Box::new(tar::Tar) as Box<dyn Pack>,
+        Box::new(tga::Tga) as Box<dyn Pack>,
         Box::new(tiff::Tiff) as Box<dyn Pack>,
+        Box::new(webp::Webp) as Box<dyn Pack>,
         Box::new(zip::Zip) as Box<dyn Pack>,
         Box::new(zstd::Zstd) as Box<dyn Pack>,
     ];
@@ -38,12 +52,19 @@ pub fn determine_compressor(mode: &str, files: &[&Path]) -> Result<Box<dyn Pack>
 
     let supported_extensions: Vec<&str> = vec![
         bmp::Bmp.extensions(),
+        dds::Dds.extensions(),
+        farbfeld::Farbfeld.extensions(),
         gif::Gif.extensions(),
+        hdr::Hdr.extensions(),
+        ico::Ico.extensions(),
         jpeg::Jpeg.extensions(),
         lzma::Lzma.extensions(),
         png::Png.extensions(),
+        pnm::Pnm.extensions(),
         tar::Tar.extensions(),
+        tga::Tga.extensions(),
         tiff::Tiff.extensions(),
+        webp::Webp.extensions(),
         zip::Zip.extensions(),
         zstd::Zstd.extensions(),
     ]
